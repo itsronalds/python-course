3f13ba78792f2119220368053793da6d51cbaf2e
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2024-03-15 23:37:50 -0400

    Add exercises

---
+++ Clase I/Ejercicios/calculadora.py
@@ -0,0 +1,9 @@
+'''
+Desarrollar una calculadora en consola que permita realizar funciones matemáticas básicas como: sumar, restar, múltiplicar, 
+dividir.
+
+Consideraciones:
+• La interfaz debe poseer las 4 opciones descritas anteriormente mediante números, además de la opción de Salir
+• Se debe validar la entrada del usuario para asegurar que eligió una opción correcta ya sea opción 1, 2, 3, 4 o 5 (salir)
+• Se debe validar la entrada del usuario durante las operaciones matemáticas, por lo que si se agrega un valor que no es un número se debe notificar el error y volver a pedir la entrada
+'''
---
+++ Clase I/Ejercicios/promedio.py
@@ -0,0 +1,9 @@
+'''
+Calcular el promedio del presupuesto de una persona en las ganancias que obtuvo durante tres meses.
+
+Consideraciones:
+• Colocar en consola los doce meses del año con la enumeración del 1 al 12
+• Validar la entrada del usuario, por lo que si se coloca un valor que no coincide con el 1-12 se debe notificar y pedir nuevamente un valor
+• Al ingresar un mes, se debe pedir la ganancia que obtuvo en USD
+• Al haber ingresado la ganancia en USD de tres meses, automáticamente se debe imprimir en pantalla el promedio de ganancia, equivalente a sacar el promedio de tres notas
+'''
